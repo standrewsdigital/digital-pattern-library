8aeb9b0f214649040c638410fe8205b0a8e13fe6
Sam Parsons <sjp27@st-andrews.ac.uk>
2014-09-19 09:26:03 +0100

    Added header and button patterns.

--- dist/javascript/st-andrews.js
+++ dist/javascript/st-andrews.js
@@ -9,10 +9,32 @@ hoverClass: 'over', // CSS class applied to the click zone onHover
 clickZone : 'div:eq(1)' // jQuery parent selector
 });
 
-$(document).ready(function(){
-$(".secondary-call").fitVids();
-});
+// $(document).ready(function(){
+// $(".secondary-call").fitVids();
+// });
 
 $(".primary-toggle").click(function(){
 	$(".navigation-bar .primary-nav").slideToggle();
+});
+
+
+/* pattern:button-toggle-ver */
+
+
+$(".button-toggle-vert").click(function(){
+	var current = $(this);
+	var target_selector = current.attr('data-target');
+	var sibling_targets_selector = current.attr('data-siblings');
+
+	$(target_selector).slideToggle().siblings(sibling_targets_selector).slideUp();
+
+	if (!current.hasClass('button-toggle-vert-up')) {
+		current.addClass('button-toggle-vert-up');
+	}
+	else {
+		current.removeClass('button-toggle-vert-up');
+	}
+	current.siblings(".button-toggle-vert").removeClass('button-toggle-vert-up');
+
+	return false;
 });
