33180c63b72be516ce5cdbae4aa2f400f1869bbe
Sam Parsons <sjp27@st-andrews.ac.uk>
2014-11-21 16:46:54 +0000

    Get info-bite working responsively.

--- src/patterns/info-bite/info-bite.js
+++ src/patterns/info-bite/info-bite.js
@@ -6,9 +6,33 @@
 
 jQuery(document).ready(function(){
 
-  $(".info-bite .bite").fitText(0.8);
-  $(".info-bite.landscape .bite").fitText(1.2);
-  $(".info-bite.portrait .bite").fitText(0.5);
+
+
+  $(".info-bite .bite").fitText(0.8,{
+      minFontSize: '28px',
+      maxFontSize: '64px'
+    }
+  );
+
+  enquire.register("screen and (min-width: 991px)", {
+    unmatch : function() {
+      $(".info-bite .bite, .info-bite.landscape .bite, .info-bite.portrait .bite")
+        .fitText(0.85,{
+            minFontSize: '28px',
+            maxFontSize: '64px'
+          });
+    },
+    match : function() {
+      $(".info-bite.landscape .bite").fitText(1.2,{
+          minFontSize: '28px',
+          maxFontSize: '64px'
+        });
+      $(".info-bite.portrait .bite").fitText(0.6,{
+          minFontSize: '28px',
+          maxFontSize: '64px'
+        });
+    }
+  });
 
 });
 
