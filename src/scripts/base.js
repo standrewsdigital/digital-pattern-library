3bfce38cd2818f44a37aef5331d558e879df03d0
Sam Parsons <sjp27@st-andrews.ac.uk>
2015-03-20 17:12:56 +0000

    A draft, testing version of the auto inserting non-breaking spaces between St and Andrews.

--- src/scripts/base.js
+++ src/scripts/base.js
@@ -9,6 +9,25 @@ $("div.image-container .image-text-box a").bigTarget({
 $('[data-toggle="popover"]').popover();
 $('[data-toggle="tooltip"]').tooltip();
 
+
+
+function addNonBreakingSpaces() {
+    var start = new Date().getTime();
+
+    var page = document.documentElement.innerHTML;
+    document.documentElement.innerHTML = page.replace(/St Andrews/g,'St&nbsp;Andrews');
+
+    var end = new Date().getTime();
+    var time = end - start;
+    console.log('Added non-breaking spaces. (' + time + 'ms)');
+}
+
+(function(){
+    addNonBreakingSpaces();    
+})(jQuery);
+
+
+
 // $(document).ready(function(){
 // $(".secondary-call").fitVids();
 // });
