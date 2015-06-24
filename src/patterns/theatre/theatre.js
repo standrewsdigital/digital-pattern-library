0a538a8ac85c0f02ec656357dda07d5baeb7463f
Sam Parsons <sjp27@st-andrews.ac.uk>
2015-06-24 17:07:07 +0100

    Added a counter and cleaned up JS considerably.

--- src/patterns/theatre/theatre.js
+++ src/patterns/theatre/theatre.js
@@ -4,72 +4,53 @@
 
         // Set default options
         var settings = $.extend({
-            // These are the defaults.
-            color:              "#556b2f",
-            backgroundColor:    "white",
-            opacity:            1,
-            title:              null,
-            titleElement:       null,
-            images:             null
+            // ...
         }, options );
 
-        
-
         var init = function() {
-            // console.log("init");
-            // console.log(this);
-            var theatreStage = $("<div id='theatre__stage'><div id='theatre__wrap'><div id='theatre__prev'><a href=''></a></div><figure id='theatre__figure'><img src='' alt=''><figcaption></figcaption></figure><div id='theatre__next'><a href=''></a></div></div><div id='theatre__close'><span class='glyphicon glyphicon-remove' aria-hidden='true'></span></div></div>");
+            // Create the theatre stage
+            // And hook up the event listeners.
+            var stage = $("<div id='theatre__stage'><div id='theatre__wrap'><div id='theatre__prev'><a href=''></a></div><figure id='theatre__figure'><img src='' alt=''><figcaption></figcaption></figure><div id='theatre__next'><a href=''></a></div></div><div id='theatre__close'><span class='glyphicon glyphicon-remove' aria-hidden='true'></span></div><div id='theatre__counter'></div></div>");
 
-            theatreStage.hide()
-                        .appendTo('body');
+            // Add new stage to the end of <body>
+            stage.hide().appendTo('body');
 
-            // Clicking any where closes the theatre.
-            // theatreStage.click(function(){
-            //     theatreStage.hide();
-            //     window.location.hash = '';
-            // });
-            // // but clicking on the image or caption does not.
-            // $('#theatre__figure img, #theatre__figure figcaption').click(function(){
-            //     return false;
-            // });
-
-            // Setup click events for next/prev buttons
+            // Set up click events for next/prev buttons
             $("#theatre__prev a, #theatre__next a").click(function(){
-                // console.log($(this).attr('href'));
                 $($(this).attr('href')).click();
                 return false;
             });
 
+            // Set up click events for close button
             $("#theatre__close").click(function(){
-                // console.log($(this).attr('href'));
-                theatreStage.hide();
+                stage.hide();
                 window.location.hash = '';
             });
 
-            // Handle key presses
-            // ==================
 
+            // Handle keyboard shortcuts
+            // =========================
 
             $("body").keyup(function(e){
                 
                 // ESC
                 if(e.which == 27){
-                    if (theatreStage.is(':visible')) {
-                        theatreStage.hide();
+                    if (stage.is(':visible')) {
+                        stage.hide();
                         window.location.hash = '';
                     }
                 }
 
                 // Right arrow (39)
                 if(e.which == 39){
-                    if (theatreStage.is(':visible')) {
+                    if (stage.is(':visible')) {
                         $("#theatre__next a").click();
                     }
                 }
 
                 // Left arrow (37)
                 if(e.which == 37){
-                    if (theatreStage.is(':visible')) {
+                    if (stage.is(':visible')) {
                         $("#theatre__prev a").click();
                     }
                 }
@@ -80,27 +61,27 @@
             // ==================
 
             // Use Hammer.js to detect swipes and gestures.
-            var hammer = new Hammer(theatreStage[0]);
+            var hammer = new Hammer(stage[0]);
             hammer.get('swipe').set({ direction: Hammer.DIRECTION_ALL });
 
             // Swipe up - closes
             hammer.on('swipeup', function(ev) {
-                if (theatreStage.is(':visible')) {
-                    theatreStage.hide();
+                if (stage.is(':visible')) {
+                    stage.hide();
                     window.location.hash = '';
                 }    
             });
 
             // Swipe right - previous
             hammer.on('swiperight', function(ev) {
-                if (theatreStage.is(':visible')) {
+                if (stage.is(':visible')) {
                     $("#theatre__prev a").click();
                 }
             });
 
             // Swipe left - next
             hammer.on('swipeleft', function(ev) {
-                if (theatreStage.is(':visible')) {
+                if (stage.is(':visible')) {
                     $("#theatre__next a").click();
                 }
             });
@@ -109,37 +90,34 @@
 
 
         var play = function(el) {
-            // this activates the theatre mode.
-            // we attach this to clicks on images.
-            // console.log('activate theatre stage');
+            // Activate theatre mode.
             var item = $(el);
             var stage = $("#theatre__stage");
+            var nextId = item.attr('data-theatre-next');
+            var prevId = item.attr('data-theatre-prev');
+            var counterText = item.attr('data-theatre-counter');
+            var caption = item.find('figcaption').text();
+            var imgSrc; // Need to conditionally find this.
 
+            // Determine whether image shown should be the 
+            // previewed thumbnail or a larger one linked to.
             if (item.find('img').parent('a').length !== 0) {
-                // console.log(item.find('img').parent('a'));
-                stage.find('img').attr('src',item.find('img').parent('a').attr('href'));            
+                imgSrc = item.find('img').parent('a').attr('href');
             }
             else {
-                // console.log('no parent link');
-                stage.find('img').attr('src',item.find('img').attr('src'));
+                imgSrc = item.find('img').attr('src');
             }
+
+            // In the theatre stage, ipdate image, caption,
+            // prev / next links and counter text.
+            stage.find('img').attr('src',imgSrc); 
+            stage.find('figcaption').text(caption);
+            $('#theatre__prev a').attr('href',"#"+prevId);
+            $('#theatre__next a').attr('href','#'+nextId);
+            $('#theatre__counter').text(counterText);
             
-            stage.find('figcaption').text(item.find('figcaption').text());
-
-            $('#theatre__prev a').hide();
-            $('#theatre__next a').hide();
-            if (item.attr('data-theatre-prev')) {
-                $('#theatre__prev a').attr('href',"#"+item.attr('data-theatre-prev'))
-                    .show();    
-            }
-
-            if (item.attr('data-theatre-next')) {
-                $('#theatre__next a').attr('href','#'+item.attr('data-theatre-next'))
-                    .show();
-            }
-
-            // update the location if current item has an ID.
-            // console.log(item);
+            // if current item has an ID 
+            // then update the address bar
             if(item.attr('id')) {
                 window.location.hash = 'theatre---' + item.attr('id');
             }
@@ -148,39 +126,32 @@
             return false;
         };
 
-
+        // Initialize the stage if not done before.
         if ( $('#theatre__stage').length === 0 ) {
             init();
-            // console.log(this);
         }
 
+        var images = this;
 
-        var current = this;
-
-
-        return current.each(function(i){
+        // Loop through each of the selected elements
+        return images.each(function(i){
+            var prev = i === 0 ? images.length-1 : i-1;
+            var next = (i+1) % images.length;
+            var counter = (i+1) + " / " + images.length;
             var item = $(this);
+
+            item.attr('data-theatre-prev', images[prev].id);
+            item.attr('data-theatre-next', images[next].id);
+            item.attr('data-theatre-counter', counter);
             item.css('cursor','pointer');
-            if (i > 0) {
-                // console.log(i,'prev', i-1);
-                // console.log(current[i-1]);
-                item.attr('data-theatre-prev', current[i-1 % current.length ].id);
-            }
-            if (i < current.length - 1) {
-                // console.log(i,'next',i+1);
-                // console.log(current[i+1]);
-                item.attr('data-theatre-next', current[i+1].id);
-            }
-            item.attr('data-theatre-title', settings.title);
-            item.attr('data-theatre-desc', settings.desc);
+
             item.click(function(){
                 return play(this);
             });
 
-            // console.log('init loading');
-            // console.log(window.location.hash);
+            // If the linked to this image in the theatre, 
+            // then load up 'play' this image.
             if ('#theatre---' + item.attr('id') === window.location.hash) {
-                // console.log(window.location.hash);
                 play(this);
             }
             
@@ -190,8 +161,9 @@
 }( jQuery ));
 
 
-
-$("figure.theatre__image").theatre({title:"asdf"});
+// By default, run theatre() on 
+// <figure> els with class 'theatre__image' 
+$("figure.theatre__image").theatre();
 
 
 
