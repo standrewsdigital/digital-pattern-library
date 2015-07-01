3021c9992f2e97e6cec458525037154e4f514117
Sam Parsons <sjp27@st-andrews.ac.uk>
2015-07-01 12:10:19 +0100

    Add code to create CSS and JS files for Study at St Andrews transition.

--- Gruntfile.js
+++ Gruntfile.js
@@ -38,6 +38,25 @@ module.exports = function(grunt) {
         ],
         dest: 'core/scripts/core.js'
       },
+      forStudy: {
+        src: [
+          /* Note: update src/docs/dependencies.md if any of these change */
+          //'src/scripts/vendor/jquery.min.js',
+          // 'src/scripts/vendor/jquery.bigtarget.js',
+          // 'src/scripts/vendor/jquery.fitvids.js',
+          // 'src/scripts/vendor/jquery.fittext.js',
+          // 'src/scripts/vendor/jquery.replacetext.js',
+          // 'src/scripts/vendor/respond.js',
+          // 'src/scripts/vendor/bootstrap.js',
+          // 'src/scripts/vendor/matchmedia.polyfill.js',
+          // 'src/scripts/vendor/matchMedia.addListener.js',
+          // 'src/scripts/vendor/enquire.js',
+          // 'src/scripts/base.js',
+          // 'src/scripts/unbreakableSpaces.js',
+          'src/patterns/header/header.js'
+        ],
+        dest: 'core/scripts/for-study.js'
+      },
       corebase: {
         src: [
           'src/scripts/base.js',
@@ -72,7 +91,8 @@ module.exports = function(grunt) {
             'src/styles/ie.scss',
             'src/styles/print.scss',
             'src/styles/screen-bootstrap-only.scss',
-            'src/styles/screen.scss'
+            'src/styles/screen.scss',
+            'src/styles/for-study.scss'
           ],
           sassDir: 'src/styles',
           importPath: 'src/patterns',
