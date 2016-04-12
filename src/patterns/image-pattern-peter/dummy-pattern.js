78f70c7aa759961a53495dd55f032365252b7808
Peter Woodbridge <pw42@st-andrews.ac.uk>
2016-04-12 11:23:08 +0100

    FEAT (image-pattern-peter) initial pattern creation

---
+++ src/patterns/image-pattern-peter/dummy-pattern.js
@@ -0,0 +1,15 @@
+
+/*********************************/
+/* BEGIN Pattern: tile           */
+/*********************************/
+
+
+// Use bigTarget to make tile image clickable.
+$(".tile h3 a").bigTarget({
+  clickZone : 'div:eq(1)' // jQuery parent selector
+});
+
+
+/*********************************/
+/* END Pattern: tile             */
+/*********************************/
