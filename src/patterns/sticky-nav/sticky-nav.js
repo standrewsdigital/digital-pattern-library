6513d372566fdd758100675dea8afe980dc943ef
ds231 <ds231@st-andrews.ac.uk>
2016-10-07 16:29:08 +0100

    FIX (sticky-nav) auditing code standards
    Audited all files for the sticky-nav pattern.

--- src/patterns/sticky-nav/sticky-nav.js
+++ src/patterns/sticky-nav/sticky-nav.js
@@ -5,39 +5,41 @@
 
 
 // Code for affixing the internal nav to the top
-    if ( $('#nav-top').length ){ var normal_top = $('#nav-top').offset().top; }
-    $('#nav-top').affix({
-      offset: {
+if ( $('#nav-top').length ) {
+    var normal_top = $('#nav-top').offset().top;
+}
+
+$('#nav-top').affix({
+    offset: {
         top: function () {
-          return normal_top;
+            return normal_top;
         },
         bottom: 0
-      }
-    });
+    }
+});
 
-
-    // Modified version of this smooth scrolling: 
+    // Modified version of this smooth scrolling:
     // https://css-tricks.com/snippets/jquery/smooth-scrolling/
 
-    $('a[href*=#]:not([href=#])').click(function() {
+$('a[href*=#]:not([href=#])').click(function() {
 
-        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') || location.hostname == this.hostname) {
+    if ( location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'' ) || location.hostname == this.hostname ) {
 
-            var target = $(this.hash);
-            target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
 
-            if (target.length) {
-                $('html,body').animate({
-                     scrollTop: target.offset().top
-                }, 500);
-                var a = $(this);
-                // a.parent().siblings().removeClass('current-menu-item');
-                // a.parent().addClass('current-menu-item');
-                //return false;
-            }
+        if ( target.length ) {
+            $('html,body').animate({
+                scrollTop: target.offset().top
+            }, 500);
+            var a = $(this);
+            // a.parent().siblings().removeClass('current-menu-item');
+            // a.parent().addClass('current-menu-item');
+            //return false;
         }
+    }
 
-    });
+});
 
 
 
