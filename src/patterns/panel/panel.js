81cb96224d8f92f108ddc3dbb178cf498242a122
Lewis Wake <wakelewis74@gmail.com>
2016-11-16 10:39:48 +0000

    FEAT (Panel grid) New pattern
    New pattern

---
+++ src/patterns/panel/panel.js
@@ -0,0 +1,15 @@
+
+/*********************************/
+/* BEGIN Pattern: panel          */
+/*********************************/
+
+
+// Use bigTarget to make tile image clickable.
+$(".tile h3 a").bigTarget({
+    clickZone : 'div:eq(1)' // jQuery parent selector
+});
+
+
+/*********************************/
+/* END Pattern: panel            */
+/*********************************/
