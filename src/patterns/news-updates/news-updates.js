f1c8cb8b349e8795b4055929da8c38785ff20f68
Stephen Evans <sde1@st-andrews.ac.uk>
2016-12-07 12:19:10 +0000

    FEAT (news-updates) Update code and print.css
    Tidy up HTML to remove extra DIV elements and add rules for print.css

---
+++ src/patterns/news-updates/news-updates.js
@@ -0,0 +1,15 @@
+
+/*********************************/
+/* BEGIN Pattern: news-updates           */
+/*********************************/
+
+
+// Use bigTarget to make tile image clickable.
+$(".news-updates h4 a").bigTarget({
+    clickZone : 'div:eq(0)' // jQuery parent selector
+});
+
+
+/*********************************/
+/* END Pattern: news-updates             */
+/*********************************/
