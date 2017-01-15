5b1d1ca8ac7f525d3bef9484a4cce685b7cd29f0
Gareth J M Saunders <gareth@garethjmsaunders.co.uk>
2017-01-15 23:33:13 +0000

    FEAT (Dummy pattern) Update following documentation rewrite
    Remove complex instructions, simply reference CONTRIBUTING.md.
    Add link to Assemble documentation for pattern template file.
    Add comments to CSS and JavaScript files to instruct developers to replace example code with their own.

--- src/patterns/dummy-pattern/dummy-pattern.js
+++ src/patterns/dummy-pattern/dummy-pattern.js
@@ -3,12 +3,12 @@
 /* BEGIN Pattern: tile           */
 /*********************************/
 
+// Replace the following with your own JavaScript
 
 // Use bigTarget to make tile image clickable.
 $(".tile h3 a").bigTarget({
     clickZone : 'div:eq(1)' // jQuery parent selector
 });
-
 
 /*********************************/
 /* END Pattern: tile             */
