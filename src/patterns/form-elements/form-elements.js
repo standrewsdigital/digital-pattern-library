346bdced343571ec08abbd852e59dc3b7b2caee5
Peter Woodbridge <pw42@st-andrews.ac.uk>
2017-01-26 16:58:07 +0000

    FEAT (form elements) conditional content
    Create form element details for conditionally revealing content.

--- src/patterns/form-elements/form-elements.js
+++ src/patterns/form-elements/form-elements.js
@@ -3,7 +3,27 @@
 /* BEGIN Pattern: form-elements  */
 /*********************************/
 
+// If 'Other' is selected in a select box, 
+// display hidden field. If not, then hide it
+$(document).ready(function() {
 
+    $('input.conditional-radio').change(
+        function() {
+            var val = $(this).val();
+
+
+            if(val === "other") {
+            	console.log("show");
+                $("#other").show();
+            }
+            else if(val !== "other") {
+            	console.log("hide");
+                $("#other").hide();
+            }
+        }   
+    );  
+
+});
 
 
 /*********************************/
