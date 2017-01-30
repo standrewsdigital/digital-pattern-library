e1739f418476ed007c08852d684c630b5229d0ec
ds231 <ds231@st-andrews.ac.uk>
2017-01-30 14:11:44 +0000

    FEAT (form elements) Conditional content
    General tidy up of all files.

--- src/patterns/form-elements/form-elements.js
+++ src/patterns/form-elements/form-elements.js
@@ -4,26 +4,23 @@
 /*********************************/
 
 // Conditionally revealing content //
-// If 'Other' is selected in a select box, 
+// If 'Other' is selected in a select box,
 // display hidden field. If not, then hide it
-$(document).ready(function() {
-    $('input.conditional-radio').change(
-        function() {
-            var val = $(this).val();
 
-            if(val === "other") {
-                $(".conditional-content").show();
-                $(".conditional-content").attr("aria-hidden", "false");
-            }
-            else if(val !== "other") {
-                $(".conditional-content").hide();
-                $(".conditional-content").attr("aria-hidden", "true");
-            }
-        }   
-    );  
+$('input.conditional-radio').change(
+    function() {
+        var val = $(this).val();
 
-});
-
+        if(val === "other") {
+            $(".conditional-content").show();
+            $(".conditional-content").attr("aria-hidden", "false");
+        }
+        else if(val !== "other") {
+            $(".conditional-content").hide();
+            $(".conditional-content").attr("aria-hidden", "true");
+        }
+    }
+);
 
 /*********************************/
 /* END Pattern: form-elements    */
