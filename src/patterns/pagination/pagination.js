1ac39d7a9a69a8e368e855c0925b62321e00da6d
Stephen Evans <sde1@st-andrews.ac.uk>
2017-01-31 14:23:25 +0000

    FEAT (Pagination) Set up pattern
    First draft of pagination

---
+++ src/patterns/pagination/pagination.js
@@ -0,0 +1,15 @@
+
+/*********************************/
+/* BEGIN Pattern: dummy pattern  */
+/*********************************/
+
+// Replace the following with your own JavaScript
+
+// Use bigTarget to make tile image clickable.
+$(".tile h3 a").bigTarget({
+    clickZone : 'div:eq(1)' // jQuery parent selector
+});
+
+/*********************************/
+/* END Pattern: dummy pattern    */
+/*********************************/
