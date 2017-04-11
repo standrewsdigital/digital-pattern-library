0be8e939d1228f409b3fad28734973ab6680d960
Aaron Lott <al244@st-andrews.ac.uk>
2017-04-11 10:44:26 +0100

    FEAT (Panel) Update panel pattern
    Add patterns page image, add bigtarget to heading and supporting CSS.

---
+++ src/patterns/panel/panel.js
@@ -0,0 +1,13 @@
+
+/*********************************/
+/* BEGIN Pattern: panel          */
+/*********************************/
+
+// Use bigTarget to make panel clickable.
+$(".panel-heading a").bigTarget({
+    clickZone : 'div:eq(1)' // jQuery parent selector
+});
+
+/*********************************/
+/* END Pattern: panel            */
+/*********************************/
