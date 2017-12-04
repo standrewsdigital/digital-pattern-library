b387bc6f62ef875163e3443c4bcc70ec87462b2a
Peter Woodbridge <pw42@st-andrews.ac.uk>
2017-12-04 16:25:56 +0000

    FEAT (photospheres) Create option
    Create the first option for the photospheres pattern.

--- src/scripts/doc.js
+++ src/scripts/doc.js
@@ -241,16 +241,13 @@ function htmlDecode(value) {
 /* BEGIN Pattern: Photospheres   */
 /*********************************/
 
-/*
-
-window.addEventListener('load', onVrViewLoad);
+window.addEventListener('load', onVrViewLoad)
 function onVrViewLoad() {
-  var vrView = new VRView.Player('#vrview', {
-    image: 'https://www.st-andrews.ac.uk/assets/university/homepage/images/hero-banner/good-university-guide-2018-hero-banner.jpg'
+  var vrView = new VRView.Player('#photosphere', {
+  image: 'https://www.st-andrews.ac.uk/assets/university/test/ABH-Bedroom.jpg'
   });
 }
 
-*/
 
 /*********************************/
 /* END Pattern: Photospheres     */
