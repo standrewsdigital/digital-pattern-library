03131746e84c48bf9a9a7e957286f95b45156767
ds231 <ds231@st-andrews.ac.uk>
2018-02-28 15:08:35 +0000

    FEAT (numerical-list) Initial setup of new pattern

---
+++ src/patterns/numerical-list/numerical-list.js
@@ -0,0 +1,11 @@
+/*********************************/
+/* BEGIN Pattern: numerical list */
+/*********************************/
+
+/* Add in any JavaScript/jQuery code for this pattern.
+ * Delete this file if JavaScript isn't required for this pattern.
+ */
+
+/*********************************/
+/* END Pattern: numerical list   */
+/*********************************/
