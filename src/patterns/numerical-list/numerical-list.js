cc5a41572ad5fb522e2b86fc35d4bf6a16ed4c10
ds231 <ds231@st-andrews.ac.uk>
2018-02-28 16:30:53 +0000

    FEAT (numerical-list) Add JavaScript
    Add JavaScript to automatically add a number to each list item.

--- src/patterns/numerical-list/numerical-list.js
+++ src/patterns/numerical-list/numerical-list.js
@@ -2,9 +2,16 @@
 /* BEGIN Pattern: numerical list */
 /*********************************/
 
-/* Add in any JavaScript/jQuery code for this pattern.
- * Delete this file if JavaScript isn't required for this pattern.
- */
+
+// Set the number for each list item.
+var count = 1;
+
+$('.list-container li').each(function() {
+
+    $('.list-container__number', this).text(count);
+    count++;
+
+});
 
 /*********************************/
 /* END Pattern: numerical list   */
