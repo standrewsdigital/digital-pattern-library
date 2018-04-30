1df0576ea1976a2f505a5d6e23d9a48222b2a969
Peter Woodbridge <pw42@st-andrews.ac.uk>
2018-04-30 16:54:35 +0100

    FEAT (featured-people) Add BigTarget
    Create JS file for BigTarget to hook into our columns

---
+++ src/patterns/navbox/featured-people.js
@@ -0,0 +1,12 @@
+/*********************************/
+/* BEGIN Pattern: featured people         */
+/*********************************/
+
+// Use bigTarget to make navbox clickable.
+$(".featured-people a").bigTarget({
+    clickZone : 'div:eq(0)' // jQuery parent selector
+});
+
+/*********************************/
+/* END Pattern: featured people           */
+/*********************************/
