2cc17d389723686965d5b205fe4b6705e3f273ad
David <drk4@st-andrews.ac.uk>
2018-05-01 09:10:00 +0100

    FEAT (news-updates) restructure patterns

--- src/patterns/news-updates/news-updates.js
+++ src/patterns/news-updates/news-updates.js
@@ -8,6 +8,12 @@ $(".news-updates h4 a").bigTarget({
     clickZone : 'div:eq(0)' // jQuery parent selector
 });
 
+// Disable hover effect on article link when sub link is the hover/focus
+$(".news-updates__category").hover(function(){
+    $(this).closest(".news-updates__article-link").toggleClass('sub-link-active');
+    console.log("hovered");
+});
+
 /*********************************/
 /* END Pattern: news-updates     */
 /*********************************/
