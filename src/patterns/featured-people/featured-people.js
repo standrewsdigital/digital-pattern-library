8b6e26206d9142e10a4238b8be0897820036ecfd
Peter Woodbridge <pw42@st-andrews.ac.uk>
2018-05-03 12:02:26 +0100

    FEAT (featured-people) CSS targets both links
    CSS code to target both a tags.

---
+++ src/patterns/featured-people/featured-people.js
@@ -0,0 +1,15 @@
+
+/***********************************/
+/* BEGIN Pattern: featured-people  */
+/***********************************/
+
+// Disable hover effect on article link when sub link is the hover/focus
+$(".person").hover(function(){
+    $(".person").toggleClass('link-active');
+});
+
+
+
+/***********************************/
+/* END Pattern: featured-people    */
+/***********************************/
