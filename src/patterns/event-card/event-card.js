f78f5c94306b24e85ee2a67b84fd3de1883e960d
Lewis Wake <wakelewis74@gmail.com>
2018-09-18 12:47:09 +0100

    FEAT (event-card) New pattern
    New event card pattern first draft

---
+++ src/patterns/event-card/event-card.js
@@ -0,0 +1,12 @@
+/*********************************/
+/* BEGIN Pattern: event-card-js   */
+/*********************************/
+
+// Use bigTarget to make tile image clickable.
+$(".tile h3 a").bigTarget({
+    clickZone : 'div:eq(1)' // jQuery parent selector
+});
+
+/*********************************/
+/* END Pattern: event-card-js     */
+/*********************************/
