aacc09c06c3c95e7a15ab41ac57db82279e94ac1
David <drk4@st-andrews.ac.uk>
2019-06-19 17:28:50 +0100

    FEAT (navigation grids) vertical flow support

--- src/patterns/navigation-grid/navigation-grid.js
+++ src/patterns/navigation-grid/navigation-grid.js
@@ -2,6 +2,13 @@
 /* BEGIN Pattern: navigation grid */
 /*********************************/
 
+$('.navigation-grid__items--vertical').each(function( index ) {
+  var items = $(this).children('.navigation-grid-item').length;
+  var rows3 =  Math.round(items / 3);
+  var rows2 =  Math.round(items / 2);
+  $(this).get(0).style.setProperty("--rows3", rows3);
+  $(this).get(0).style.setProperty("--rows2", rows2);
+});
 
 /*********************************/
 /* END Pattern: navigation grid   */
