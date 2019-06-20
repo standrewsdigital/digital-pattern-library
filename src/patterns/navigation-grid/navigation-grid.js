09b426ca55f085daf7827dde5a462b4d77487417
David <drk4@st-andrews.ac.uk>
2019-06-20 11:43:31 +0100

    FEAT (navigation grid) updates
    - column option
    - spacing fixed

--- src/patterns/navigation-grid/navigation-grid.js
+++ src/patterns/navigation-grid/navigation-grid.js
@@ -4,8 +4,8 @@
 
 $('.navigation-grid__items--vertical').each(function( index ) {
   var items = $(this).children('.navigation-grid-item').length;
-  var rows3 =  Math.round(items / 3);
-  var rows2 =  Math.round(items / 2);
+  var rows3 =  Math.ceil(items / 3);
+  var rows2 =  Math.ceil(items / 2);
   $(this).get(0).style.setProperty("--rows3", rows3);
   $(this).get(0).style.setProperty("--rows2", rows2);
 });
