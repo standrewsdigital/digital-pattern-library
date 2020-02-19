eef4bdee1c0fea4be5f782aedb44bc05666d933b
David <drk4@st-andrews.ac.uk>
2020-02-19 16:23:01 +0000

    FEAT (a11y)

--- src/patterns/panel/panel.js
+++ src/patterns/panel/panel.js
@@ -4,9 +4,9 @@
 /*********************************/
 
 // Use bigTarget to make panel clickable.
-$(".panel-heading a").bigTarget({
+/*$(".panel-heading a").bigTarget({
     clickZone : 'div:eq(0)' // jQuery parent selector
-});
+});*/
 
 /*********************************/
 /* END Pattern: panel            */
