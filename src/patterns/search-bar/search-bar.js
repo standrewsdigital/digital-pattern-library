ef92c3e2787b064dbe4bbd5a7e9175e3f0499e82
David <drk4@st-andrews.ac.uk>
2020-12-03 16:50:57 +0000

    FEAT (horizontal links) new pattern
    Separated out to help search bar and hero banner

--- src/patterns/search-bar/search-bar.js
+++ src/patterns/search-bar/search-bar.js
@@ -1,14 +1,3 @@
-
-$(document).on('click', '.horizontal-list__toggle', function(){
-	$(this).next('ul').slideToggle(200, "linear");
-    $(this).toggleClass( "open" ); // add class for styling.
-    // Set the aria properties.
-    var state = $(this).find('.horizontal-list__toggle-button').attr('aria-expanded') === 'false' ? true : false;
-   	$(this).find('.horizontal-list__toggle-button').attr('aria-expanded', state);
-    $(this).next('ul').attr('aria-hidden', !state);
-    return false; // disable default event.
-});
-
     // Avoid submitting search that only produces on page results using entered keywords
     $(".form-nosubmit").submit(function(e) {
         e.preventDefault();
