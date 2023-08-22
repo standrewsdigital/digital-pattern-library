5a3d71cd06635b68a05aec040f5b858c7c51bb2e
teovu <tvu1@st-andrews.ac.uk>
2023-08-22 08:36:59 +0100

    FIX (Accordion mobile) Accordion mobile javascript working, minus scss

--- src/patterns/accordion-mobile/accordion-mobile.js
+++ src/patterns/accordion-mobile/accordion-mobile.js
@@ -4,50 +4,50 @@
 
 
 // Assign ARIA roles for every accordion item
-var accordion_increment = 0;
+var accordion_mobile_increment = 0;
 
 
-$('.accordion-item').each(function() {
+$('.accordion-mobile-item').each(function() {
     var $this = $(this);
-    var id = 'accordion-item-' + accordion_increment;
-    var manualaria = $this.find('.accordion-item__toggle-button').get(0).hasAttribute('aria-expanded');
+    var id = 'accordion-mobile-item-' + accordion_mobile_increment;
+    var manualaria = $this.find('.accordion-mobile-item__toggle-button').get(0).hasAttribute('aria-expanded');
     if (!manualaria) {
-        $this.find('.accordion-item__toggle-button').attr({
+        $this.find('.accordion-mobile-item__toggle-button').attr({
             'aria-expanded': 'false',
             'aria-controls': id
         });
-        $this.find('.accordion-item__content').attr({
+        $this.find('.accordion-mobile-item__content').attr({
             'id': id,
             'aria-hidden': 'true'
         });
     }
-    accordion_increment++;
+    accordion_mobile_increment++;
 });
 
 
 // Individual accordion item toggle
-$(document).on('click', '.accordion-item__toggle', function() {
+$(document).on('click', '.accordion-mobile-item__toggle', function() {
     var $this = $(this);
-    var $content = $this.next('.accordion-item__content');
-    var state = $this.find('.accordion-item__toggle-button').attr('aria-expanded') === 'false' ? true : false;
+    var $content = $this.next('.accordion-mobile-item__content');
+    var state = $this.find('.accordion-mobile-item__toggle-button').attr('aria-expanded') === 'false' ? true : false;
 
 
     // Close other accordions
-    $('.accordion-item__content').not($content).slideUp(200, "linear");
-    $('.accordion-item__content').not($content).prev().find('.accordion-item__toggle-button').attr('aria-expanded', 'false');
+    $('.accordion-mobile-item__content').not($content).slideUp(200, "linear");
+    $('.accordion-mobile-item__content').not($content).prev().find('.accordion-mobile-item__toggle-button').attr('aria-expanded', 'false');
 
 
     // Toggle visibility of other accordion item toggles
     if (state) {
-        $('.accordion-item__toggle').not($this).hide();
+        $('.accordion-mobile-item__toggle').not($this).hide();
     } else {
-        $('.accordion-item__toggle').show();
+        $('.accordion-mobile-item__toggle').show();
     }
 
 
     // Toggle current accordion
     $content.slideToggle(200, "linear");
-    $this.find('.accordion-item__toggle-button').attr('aria-expanded', state.toString());
+    $this.find('.accordion-mobile-item__toggle-button').attr('aria-expanded', state.toString());
     $content.attr('aria-hidden', (!state).toString());
     return false;
 });
